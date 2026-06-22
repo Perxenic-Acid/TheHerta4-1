825358e3ef27287a28302046fa0e11250502d499
Administrator <Administrator@DESKTOP-BBNRNSR>
2026-06-22 23:57:34 +0800

    1

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ bl_info = {
     "name": "TheHerta4",
     "description": "Blender Plugin of SSMT4",
     "blender": (3, 6, 0),
-    "version": (4, 1, 34),
+    "version": (4, 1, 35),
     "location": "View3D",
     "category": "Generic"
 }
