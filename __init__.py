d3843836038c83b87acb3129b5e87fe51da3eac3
example <example@example.com>
2026-08-05 22:41:50 +0800

    版本号更新

--- __init__.py
+++ __init__.py
@@ -37,7 +37,7 @@ bl_info = {
     "name": "TheHerta4",
     "description": "Blender Plugin of SSMT4",
     "blender": (3, 6, 0),
-    "version": (4, 1, 37),
+    "version": (4, 1, 38),
     "location": "View3D",
     "category": "Generic"
 }
